be4dd9996d698c13c5971942ee833157c35a7b0b
nooryghost <109327061+nooryghost@users.noreply.github.com>
2022-09-20 21:04:54 +0430

    like added to posts

---
+++ website/static/js/index.js
@@ -0,0 +1,18 @@
+function like(postId){
+    const likeCount = document.getElementById(`likes-count-${postId}`);
+    const likeButton = document.getElementById(`like-button-${postId}`);
+
+    fetch(`/like-post/${postId}`, { method: "POST" })
+    .then((res) => res.json())
+    .then((data) => {
+        likeCount.innerHTML = data["likes"];
+        if (data["liked"]){
+            likeButton.className = "fas fa-thumbs-up";
+        }
+        else{
+            likeButton.className = "far fa-thumbs-up";
+        }
+    })
+    .catch((e) => alert("Could not like post!")); 
+
+}
