497f34e567624966f0f7e40fc6673b44c2bd2cc1
nooryghost <109327061+nooryghost@users.noreply.github.com>
2022-09-26 19:26:16 +0330

    profile fixed

--- website/views.py
+++ website/views.py
@@ -138,7 +138,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path =os.path.join(views.root_path, "static/pictures", picture_fn)
+    picture_path = os.path.join(views.root_path, "static/pictures", picture_fn)
 
     output_size = (125,125)
     image = Image.open(form_picture)
